6d913516d62d564c0d620ebd4a1c406d5d295b4b
greedysky <greedysky@163.com>
2021-06-06 21:45:27 +0800

    refactor: Optimized input module

--- v2mhelper.h
+++ v2mhelper.h
@@ -20,13 +20,13 @@
 #define V2MHELPER_H
 
 #include <QFile>
-#include "v2mplayer.h"
+#include <libv2m/v2mplayer.h>
 
 typedef struct {
     uint8_t *tune;
     int bitrate;
-    V2MPlayer *player;
-} v2m_info;
+    V2MPlayer *input;
+} decode_info;
 
 /*!
  * @author Greedysky <greedysky@163.com>
@@ -52,7 +52,7 @@ public:
 
 private:
     QString m_path;
-    v2m_info *m_info;
+    decode_info *m_info;
 
 };
 
